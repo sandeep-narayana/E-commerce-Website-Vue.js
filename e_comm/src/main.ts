32c547535f2a8a40e27ea54406413b2ac665ddd8
Sandeep Rawat <132456591+sandeep-narayana@users.noreply.github.com>
2023-08-31 14:50:45 +0530

    using element plus instead of tailwind ui

--- e_comm/src/main.ts
+++ e_comm/src/main.ts
@@ -3,5 +3,8 @@ import App from './App.vue'
 import store from './store'
 import router from './Route'
 import './index.css'
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
 
-createApp(App).use(router).use(store).mount('#app')
+
+createApp(App).use(router).use(store).use(ElementPlus).mount('#app')
